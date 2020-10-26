c3b1e8b04ebdf9f5021cdcede8d397e1a5409911
Bashar Khouri <Bashar.19002@hotmail.com>
2020-10-26 23:54:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dropoflife/ui/profile/ProfileFragment.java
@@ -0,0 +1,22 @@
+package com.example.dropoflife.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.dropoflife.R;
+
+public class ProfileFragment extends Fragment {
+
+    private ProfileViewModel profileViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile,container,false);
+    }
+}
