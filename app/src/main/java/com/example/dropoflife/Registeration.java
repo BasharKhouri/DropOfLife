75897ad5e92c5205da6afcbb6b059b645bf07a39
Bashar Khouri <Bashar.19002@hotmail.com>
2020-10-29 13:17:57 +0300

    added regsetry and sign in

---
+++ app/src/main/java/com/example/dropoflife/Registeration.java
@@ -0,0 +1,14 @@
+package com.example.dropoflife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registeration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registeration);
+    }
+}
