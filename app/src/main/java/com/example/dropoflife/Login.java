eec08bb9f2ba3c2494ad6845a8e89f769c19654d
Hassan Wael <hassanwael1999@gmail.com>
2020-10-30 18:43:27 +0200

    Row Edit

--- app/src/main/java/com/example/dropoflife/Login.java
+++ app/src/main/java/com/example/dropoflife/Login.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 
     public void goToRegister(View view) {
