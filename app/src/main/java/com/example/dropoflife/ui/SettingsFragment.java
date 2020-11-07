aa789623d5ecde3d7e72e6edf5027d28bb1d90d4
Hassan Wael <hassanwael1999@gmail.com>
2020-11-07 13:23:30 +0200

    DARK MODE

--- app/src/main/java/com/example/dropoflife/ui/SettingsFragment.java
+++ app/src/main/java/com/example/dropoflife/ui/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.dropoflife.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -17,12 +18,18 @@ import com.example.dropoflife.Classes.User;
 import com.example.dropoflife.Login;
 import com.example.dropoflife.MainActivity;
 import com.example.dropoflife.R;
+import com.example.dropoflife.Splashscreen;
 import com.google.firebase.auth.FirebaseAuth;
+
+import static android.content.Context.CONTEXT_IGNORE_SECURITY;
+import static com.example.dropoflife.Login.SHARED_NAME;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     Preference logoutET;
     SwitchPreferenceCompat darkMode;
+    public boolean test;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
@@ -44,20 +51,30 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             });
         }
 
-        darkMode.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                boolean test = darkMode.isChecked();
 
-                if (!test){
+        assert darkMode != null;
+        darkMode.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                test = darkMode.isChecked();
+                if (test){
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
                 }else{
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
-
-                return true;
+                saveDark(test);
+                return false;
             }
         });
+
+    }
+    public void saveDark(boolean dark_theme){
+        SharedPreferences shared = getActivity().getSharedPreferences(SHARED_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit= shared.edit();
+        edit.putBoolean("DARK",dark_theme);
+        edit.apply();
+
 
     }
 
