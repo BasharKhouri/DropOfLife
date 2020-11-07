7a9cc11ae310339378ea9658f83062d6b370e3d5
Hassan Wael <hassanwael1999@gmail.com>
2020-11-07 15:42:11 +0200

    Revert "DARK MODE"
    This reverts commit 2cd2c5adf917c2656032e2234a5234415f866d83.

--- app/src/main/java/com/example/dropoflife/Splashscreen.java
+++ app/src/main/java/com/example/dropoflife/Splashscreen.java
@@ -1,19 +1,27 @@
 package com.example.dropoflife;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.example.dropoflife.ui.SettingsFragment;
+
+import static com.example.dropoflife.Login.SHARED_NAME;
 
 public class Splashscreen extends AppCompatActivity {
     private static int SPLASH_TIMEOUT = 100;
+    static boolean dark_theme=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
-        new Handler().postDelayed(new Runnable() {
+               new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent welcomeIntent = new Intent(Splashscreen.this, Login.class);
@@ -21,5 +29,22 @@ public class Splashscreen extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIMEOUT);
+               loadDark();
+
     }
+
+
+    public  void loadDark(){
+        SharedPreferences shared = getSharedPreferences(SHARED_NAME,Context.MODE_PRIVATE);
+        dark_theme = shared.getBoolean("DARK",false);
+
+        if (dark_theme){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+    }
+
 }
