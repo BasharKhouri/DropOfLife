ef575e8ddb04f9234683511699e2556fde14e709
Bashar Khouri <Bashar.19002@hotmail.com>
2020-11-11 20:56:37 +0200

    mad addHospitalxml

---
+++ app/src/main/java/com/example/dropoflife/AddHospital.java
@@ -0,0 +1,19 @@
+package com.example.dropoflife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class AddHospital extends AppCompatActivity {
+
+    EditText hospitalNameET
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hospital);
+
+
+    }
+}
--- app/src/main/java/com/example/dropoflife/ui/Admin.java
+++ app/src/main/java/com/example/dropoflife/ui/Admin.java
@@ -8,22 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.dropoflife.Classes.User;
+import com.example.dropoflife.MainActivity;
 import com.example.dropoflife.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Admin#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class Admin extends Fragment {
-
+        User user;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_admin, container, false);
 
-
+         user = MainActivity.user;
 
         return  view;
     }
