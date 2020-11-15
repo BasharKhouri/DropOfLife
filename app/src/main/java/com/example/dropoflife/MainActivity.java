75a2ef0c3026f8e73772871cb958f94ba5afc151
Bashar Khouri <Bashar.19002@hotmail.com>
2020-11-15 10:56:46 +0200

    added admin view to the nave bar

--- app/src/main/java/com/example/dropoflife/MainActivity.java
+++ app/src/main/java/com/example/dropoflife/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.dropoflife.Classes.Roles;
 import com.example.dropoflife.Classes.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,12 +26,12 @@ import androidx.navigation.ui.NavigationUI;
  * author Bashar Khouri,Hassan wael ,Bashar Nimri
  */
 public class MainActivity extends AppCompatActivity {
-
+    private Roles role ;
     public static User user;
     private FirebaseAuth mAuth;
     public static FirebaseUser firebaseUser;
     FirebaseFirestore fStore = FirebaseFirestore.getInstance();
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
                     user = value.toObject(User.class);
+                    role = user.getRole();
                 }
             })) {
 
@@ -56,22 +58,33 @@ public class MainActivity extends AppCompatActivity {
         }catch (Exception ignore){
 
         }
+            try {
+                setContentView(R.layout.activity_main);
+                BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+                // Passing each menu ID as a set of Ids because each
+                // menu should be considered as top level destinations.
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        Intent intent = getIntent();
-        user= (User) intent.getSerializableExtra("User");
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_profile, R.id.navigation_notifications,R.id.navigation_settings1)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-
-
+                     intent = getIntent();
+                if (role.equals(null) || !role.equals(new Roles(1))) {
+                    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                            R.id.navigation_home, R.id.navigation_profile, R.id.navigation_notifications, R.id.navigation_settings1)
+                            .build();
+                findViewById(R.id.navigation_AdminView).setVisibility(View.GONE);
+                    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+                    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+                    NavigationUI.setupWithNavController(navView, navController);
+                }else if (role.equals( new Roles(1))){
+                    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                            R.id.navigation_home, R.id.navigation_profile, R.id.navigation_notifications, R.id.navigation_settings1,R.id.navigation_AdminView)
+                            .build();
+                    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+                    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+                    NavigationUI.setupWithNavController(navView, navController);
+                }
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
 
     }
 }
